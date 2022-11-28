f62e14382d7920234b950e4fbbe41363e49f138e
steelsnake93 <nikolanikotasev@protonmail.com>
2022-11-28 19:20:00 +0100

    navigation area + spacing, flex, responsive classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       md: '768px',
       lg: '976px',
       xl: '1440px',
-    }
+    },
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
@@ -18,7 +18,7 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
-      }
+      },
     },
   },
   plugins: [],
